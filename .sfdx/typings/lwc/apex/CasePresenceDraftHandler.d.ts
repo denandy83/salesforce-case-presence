f40e2488a75ba55b714b0c419d1aa898d5562248
denandy83 <andy.cassiers@gmail.com>
2026-01-16 17:07:41 +0100

    deleted drafthandler because we don't actually use it

--- .sfdx/typings/lwc/apex/CasePresenceDraftHandler.d.ts
+++
@@ -1,6 +0,0 @@
-declare module "@salesforce/apex/CasePresenceDraftHandler.currentUserHasDraftEmails" {
-  export default function currentUserHasDraftEmails(param: {caseId: any}): Promise<any>;
-}
-declare module "@salesforce/apex/CasePresenceDraftHandler.currentUserHasAnyDrafts" {
-  export default function currentUserHasAnyDrafts(param: {caseId: any}): Promise<any>;
-}
